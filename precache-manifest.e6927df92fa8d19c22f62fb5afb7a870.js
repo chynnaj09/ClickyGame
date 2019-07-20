c5c53053ab45a3cad5e7cc4af39218f727fdaf51
Chynna Johnson <chynnajohnson@Chynnas-MBP.attlocal.net>
2019-07-20 16:03:36 -0500

    Updates

---
+++ precache-manifest.e6927df92fa8d19c22f62fb5afb7a870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c61abea1ed67e573c920a2a1ba8f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b6084a99cc006f6feb",
+    "url": "/static/css/main.152c385d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "b0b6084a99cc006f6feb",
+    "url": "/static/js/main.414ebbce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
